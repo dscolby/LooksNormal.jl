f5504d658efa6bf984ae183d0ace66b47a5f16de
Documenter.jl <documenter@juliadocs.github.io>
2022-10-30 03:09:54 +0000

    build based on 6a83555

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = LooksNormal","category":"page"},{"location":"#LooksNormal","page":"Home","title":"LooksNormal","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for LooksNormal.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [LooksNormal]","category":"page"}]
+}
